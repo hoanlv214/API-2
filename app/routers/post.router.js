42ec0b586deddc342fa37a054c99b732c655c956
vanlinh00 <82046592+vanlinh00@users.noreply.github.com>
2021-10-11 00:14:59 +0700

    edit post ok

--- app/routers/post.router.js
+++ app/routers/post.router.js
@@ -6,19 +6,21 @@ module.exports = function(router)
     //API  1  add_post ok
    router.post('/post/add_post',postController.add_post);
    // get_post 2 ok
-   router.get('/post/get_post',postController.get_post);
+   router.post('/post/get_post',postController.get_post);
 
-   //API get_list_posts 3
-   router.get('/post/list',postController.get_list_posts);
+   //API get_list_posts 3  gần ok
+   router.post('/post/get_list_posts',postController.get_list_posts);
+  
+   //check_new_item 4   ok
+  router.post('/post/check_new_item',postController.check_new_item);
 
-   //edit_post 4
-   router.get('/post/edit_post',postController.edit_post);
+   //edit_post 5 ok
+   router.put('/post/edit_post',postController.edit_post);
 
-   //delete_post 5
+   //delete_post 6
    router.get('/post/delete_post',postController.delete_post);
 
-   //check_new_item 6
-   router.get('/post/check_new_item',postController.check_new_item);
+
 
       //git push -u
 }
